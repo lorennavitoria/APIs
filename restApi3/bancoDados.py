1033f3ca7cfdb313843b3f62ab9ead80b105b14f
lorennavitoria <61290879+lorennavitoria@users.noreply.github.com>
2022-06-02 01:53:52 -0300

    Create bancoDados.py

---
+++ restApi3/bancoDados.py
@@ -0,0 +1,9 @@
+from restApi.construcaoApiVersao3.models.conversorJson import bancoJson
+
+bancoDados = [
+
+    bancoJson('Joao', 31, 'M', 812232),
+    bancoJson('Maria', 29, 'F', 458545),
+    bancoJson('José', 40, 'M', 4855451)
+
+]
