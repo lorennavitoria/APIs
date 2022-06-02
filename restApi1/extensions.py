2c0c61de98a1260799f6a884d266d80bc4f182cd
lorennavitoria <61290879+lorennavitoria@users.noreply.github.com>
2022-06-02 01:46:02 -0300

    Create extensions.py

---
+++ restApi1/extensions.py
@@ -0,0 +1,10 @@
+from restApi.construcaoApiVersao1.models.ngo import NGO
+
+# este arquivo é só para criar o banco de dados, coloca-se dentro de NGO, para ser convertido em Json
+
+db = [
+
+    NGO('Lar Pequeno Leão', 'Milton Bigucci', 'Acolhimento'),
+    NGO('Lar da Mamãr Clory', 'Clory Fagundes de Marques', 'Associação Beneficente'),
+    NGO('Instituição Assitencial Meimei', 'Centro Espírta Obreiros do Senhor', 'Serviço Assistencial')
+]
